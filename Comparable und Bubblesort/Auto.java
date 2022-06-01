08fbbef76a805e19810712e83b12266669ced0b7
Mehmet Yazlak <mehmet.yazlak@thyssenkrupp.com>
2022-06-01 21:14:46 +0200

    Vergleich Collections.sort und eigener Bubblesort

--- Comparable und Bubblesort/Auto.java
+++ Comparable und Bubblesort/Auto.java
@@ -1,56 +1,52 @@
-public class Auto implements Comparable<Auto>{
-    
+public class Auto implements Comparable<Auto> {
+
     int Baujahr;
     int KMStand;
     String Marke;
-    String Modell;
 
-    Auto(String Marke,String Modell,int Baujahr, int KMStand){
+    Auto(String Marke, int Baujahr, int KMStand) {
 
         this.Marke = Marke;
         this.KMStand = KMStand;
-        this.Modell = Modell;
         this.Baujahr = Baujahr;
 
     }
 
     @Override
+    public String toString() {
+
+        String format = String.format("%-13s %-13d %dkm", this.Marke, this.Baujahr, this.KMStand);
+        return format;
+    }
+
+    // Baujahr > KMStand > Marke > Modell
+
+    @Override
     public int compareTo(Auto o) {
-        if(this.Baujahr > o.Baujahr){
+        if (this.Baujahr > o.Baujahr) {
             return 1;
         }
-        if(this.Baujahr < o.Baujahr){
+        if (this.Baujahr < o.Baujahr) {
             return -1;
-        }
-        else{
-            if(this.KMStand > o.KMStand){
+        } else {
+            if (this.KMStand > o.KMStand) {
                 return 1;
             }
-            if(this.KMStand < o.KMStand){
+            if (this.KMStand < o.KMStand) {
                 return -1;
-            }
-            else{
-                if(this.Marke.compareTo(o.Marke) > 0){
+            } else {
+                if (this.Marke.compareTo(o.Marke) > 0) {
                     return 1;
                 }
-                if(this.Marke.compareTo(o.Marke) < 0){
+                if (this.Marke.compareTo(o.Marke) < 0) {
                     return -1;
-                } 
-                else{
-                    if(this.Modell.compareTo(o.Modell) > 0){
-                        return 1;
-                    }
-                    if(this.Modell.compareTo(o.Modell) < 0){
-                        return -1;
-                    } 
-                    else{
-                        return 0;
-                    }
+                } else {
+
+                    return 0;
                 }
             }
-            
         }
-        
+
     }
 
 }
--- Comparable und Bubblesort/Main.java
+++ Comparable und Bubblesort/Main.java
@@ -1,39 +1,77 @@
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
+import java.util.Date;
 
-import javax.swing.text.View;
+public class Main {
 
-public class Main{
-
-public static void main(String[] args) {
-    
-ArrayList<Auto> uListe = new ArrayList<Auto>();
-uListe.add(new Auto("VW", "Polo", 2000, 200000));
-
-uListe.add(new Auto("VW", "Golf", 2000, 200000));
-
-uListe.add(new Auto("Audi", "A4", 2000, 200000));
-
-uListe.add(new Auto("Hyundai", "i30", 2001, 210000));
-Random rand = new Random();
-
-for(int i = 0; i< 1000; i++){}
-
-
-
-for(int i =0; i < uListe.size()-1;i++){
-
-    
-    
-    if(uListe.get(i).compareTo(uListe.get(i+1))>0)
-    {
-        Auto temp = uListe.get(i);
-        uList = uListe.get(i);
-        uListe.get(i) = temp;
-
+    public static Auto createAuto() {
+        Random rand = new Random();
+        String marken[] = new String[] { "Audi", "BMW", "Mercedes", "VW" };
+        String marke = marken[rand.nextInt((4 - 0) + 0) + 0];
+        int baujahr = rand.nextInt(2022 - 1950 + 1) + 1950;
+        int kmstand = rand.nextInt(300000 - 0 + 1) + 0;
+        return new Auto(marke, baujahr, kmstand);
     }
 
+    public static void printList(ArrayList<Auto> list) {
+        System.out.printf("%-13s %-13s %s \n", "Marke", "Baujahr", "KMStand");
 
+        for (Auto a : list) {
+            System.out.println(a.toString());
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int cars = 1000;
+        ArrayList<Auto> listeBubbleSort = new ArrayList<>();
+        ArrayList<Auto> listeCollectionSort = new ArrayList<>();
+
+        for (int i = 0; i < cars; i++) {
+            listeBubbleSort.add(createAuto());
+        }
+        for (int i = 0; i < cars; i++) {
+            listeCollectionSort.add(createAuto());
+        }
+
+        // BubbleSort
+        LocalDateTime beforeBubble = LocalDateTime.now();
+
+        for (int j = 0; j < listeBubbleSort.size(); j++) {
+            for (int i = 0; i < listeBubbleSort.size() - 1; i++) {
+
+                Auto temp = listeBubbleSort.get(i);
+                if (listeBubbleSort.get(i).compareTo(listeBubbleSort.get(i + 1)) > 0) {
+                    listeBubbleSort.set(i, listeBubbleSort.get(i + 1));
+                    listeBubbleSort.set(i + 1, temp);
+                }
+
+            }
+        }
+
+        LocalDateTime afterBubble = LocalDateTime.now();
+        long diff = ChronoUnit.NANOS.between(beforeBubble, afterBubble);
+
+        System.out.println("Anzahl der Autos in der Liste: " + cars);
+
+        System.out.printf("%-20s %15dns \n", "Bubble Sort", diff);
+
+        // printList(listeBubbleSort);
+
+        // Collection Sort
+        LocalDateTime beforeCollection = LocalDateTime.now();
+
+        Collections.sort(listeCollectionSort);
+
+        LocalDateTime afterCollection = LocalDateTime.now();
+        long diff2 = ChronoUnit.NANOS.between(beforeCollection, afterCollection);
+
+        System.out.printf("%-20s %15dns \n", "Collection Sort", diff2);
+
+        // printList(listeCollectionSort);
+
+    }
 }
-
-}}
